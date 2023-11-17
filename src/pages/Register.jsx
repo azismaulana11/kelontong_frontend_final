53a74a2bec28183291c23a2540ca558744cce4c5
azismaulana1883 <azis.maulana1883@gmail.com>
2023-11-17 16:34:16 +0700

    penambahan beberapa validasi

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,85 @@
-import { useEffect } from 'react'
-import { Link  } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import imgLogo from '../assets/img/logo.jpeg';
+import { register } from '../services/index';
+import Swal from 'sweetalert2';
 
 function Register() {
-useEffect(() => {
-  document.title = 'Regiter';
-}, []);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password_2: '',
+    alamat: '',
+    phone_number: '',
+    role: 'customer',
+  });
+
+  const handleInputChange = (e) => {
+  if (e.target.id === 'role') {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value || 'customer',
+    });
+  } else if (e.target.id === 'phone_number') {
+    const numericValue = e.target.value.replace(/\D/g, '');
+    setFormData({
+      ...formData,
+      [e.target.id]: numericValue,
+    });
+  } else {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  }
+};
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  if (formData.password !== formData.password_2) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Password does not match. Please check again.',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+
+  try {
+    const response = await register(formData);
+
+    if (response.success) {
+      Swal.fire({
+        icon: 'success',
+        title: 'Registration successful!',
+        showConfirmButton: false,
+        timer: 1500,
+      }).then(() => {
+        navigate('/login');
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Registration failed',
+        text: response.message,
+        showConfirmButton: true,
+      });
+    }
+  } catch (error) {
+    console.error('Error during registration:', error);
+    Swal.fire({
+      icon: 'error',
+      title: 'Registration failed. Please try again.',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+};
+
   return (
-    <>
     <div className="container vh-100">
       <div className="row h-100 justify-content-center align-items-center">
         <div className="col-lg-4 col-sm-12 col-md-12">
@@ -17,21 +89,20 @@ useEffect(() => {
                 <div className="col-lg-12 col-md-12 col-sm-12 text-center">
                   <h1 className="text-primary fw-bold">
                     <Link to="/">
-                        <a className="navbar-brand" href="#">
-                          <img src={imgLogo} alt="Kelontong" width="300" height="150"/>
-                        </a>
-                      </Link>
+                      <a className="navbar-brand" href="#">
+                        <img src={imgLogo} alt="Kelontong" width="300" height="150" />
+                      </a>
+                    </Link>
                   </h1>
                 </div>
               </div>
             </div>
 
-            {/* <!-- Register Form --> */}
-
+            {/* Register Form */}
             <div className="container">
               <div className="row">
                 <div className="col">
-                  <form action="" method="POST">
+                  <form onSubmit={handleSubmit}>
                     <div className="container-fluid">
                       <div className="row">
                         <div className="col-12">
@@ -45,9 +116,11 @@ useEffect(() => {
                               <input
                                 type="text"
                                 className="form-control"
-                                id="nama"
+                                id="name"
                                 placeholder="Full Name"
                                 required
+                                value={formData.name}
+                                onChange={handleInputChange}
                               />
                             </div>
                           </div>
@@ -57,8 +130,36 @@ useEffect(() => {
                                 type="email"
                                 className="form-control"
                                 id="email"
-                                placeholder="email"
+                                placeholder="Email"
                                 required
+                                value={formData.email}
+                                onChange={handleInputChange}
+                              />
+                            </div>
+                          </div>
+                          <div className="col-lg-6 col-md-6 col-sm-12">
+                            <div className="mb-3">
+                              <input
+                                type="text"
+                                className="form-control"
+                                id="phone_number"
+                                placeholder="No.HP"
+                                required
+                                value={formData.phone_number}
+                                onChange={handleInputChange}
+                              />
+                            </div>
+                          </div>
+                          <div className="col-lg-6 col-md-6 col-sm-12">
+                            <div className="mb-3">
+                              <input
+                                type="alamat"
+                                className="form-control"
+                                id="alamat"
+                                placeholder="alamat"
+                                required
+                                value={formData.alamat}
+                                onChange={handleInputChange}
                               />
                             </div>
                           </div>
@@ -70,6 +171,10 @@ useEffect(() => {
                                 id="password"
                                 placeholder="Your Password"
                                 required
+                                minLength="8"
+                                maxLength="8"
+                                value={formData.password}
+                                onChange={handleInputChange}
                               />
                             </div>
                           </div>
@@ -81,9 +186,20 @@ useEffect(() => {
                                 id="password_2"
                                 placeholder="Confirm Password"
                                 required
+                                minLength="8"
+                                maxLength="8"
+                                value={formData.password_2}
+                                onChange={handleInputChange}
                               />
                             </div>
                           </div>
+                          {/* Hidden Input for Role */}
+                          <input
+                            type="hidden"
+                            id="role"
+                            value={formData.role}
+                            onChange={handleInputChange}
+                          />
                           <button
                             type="submit"
                             className="btn btn-primary"
@@ -102,8 +218,7 @@ useEffect(() => {
         </div>
       </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Register
+export default Register;
--- src/services/index.js
+++ src/services/index.js
@@ -1,45 +1,17 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const BASE_URL_API = "https://652760d5917d673fd76d9d06.mockapi.io/api/v1/product-list-kelontong/product";
+const BASE_URL_API_REGISTER = "http://localhost:7600/api/v1/auth";
 
-const BASE_URL_API_LOGIN = "https://pear-successful-mackerel.cyclic.app/api/v1/auth/login";
-
-console.log(BASE_URL_API)
-
-const getProductList = async () => {
-    try {
-        const url = `${BASE_URL_API}`;
-        const response = await axios.get(url);
-        return response.data;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-const login = async (username, password) => {
-    try {
-        const url_login = `${BASE_URL_API_LOGIN}`;
-        const response = await axios.post(url_login, { username, password });
-        return response.data;
-    } catch (error) {
-        if (error.response) {
-            // Tangkap status code dari respon yang diterima
-            const statusCode = error.response.status;
-            console.log(`HTTP status code: ${statusCode}`);
-
-            // Tangkap pesan error jika ada
-            const errorMessage = error.response.data.message;
-            console.log(`Error message: ${errorMessage}`);
-        } else {
-            console.log('Error:', error.message);
-        }
-
-        // Dilemparkan kembali agar dapat dihandle di komponen yang memanggil fungsi login
-        throw error;
-    }
+const register = async (userData) => {
+  try {
+    const response = await axios.post(`${BASE_URL_API_REGISTER}/register`, userData);
+    return response.data;
+  } catch (error) {
+    console.error('Error during registration:', error);
+    throw error;
+  }
 };
 
 export {
-getProductList,
-login
-}
+  register
+};
