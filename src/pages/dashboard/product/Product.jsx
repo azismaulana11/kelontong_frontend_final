5bf6d284dff21a61f14ddd5ee3cfdda0ae194548
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-21 21:50:13 +0700

    Add API call to fetch products

--- src/pages/dashboard/product/Product.jsx
+++ src/pages/dashboard/product/Product.jsx
@@ -2,6 +2,21 @@ import HeadingTitle from "../../../components/dashboard/HeadingTitle";
 import DashboardLayout from "../../../layout/DashboardLayout";
 
 export default function Product() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const response = await axios.get("http://localhost:5500/api/v1/products");
+                setProducts(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getProducts();
+    }, []);
+
+    
     return (
         <DashboardLayout>
             <HeadingTitle title="Produk">
