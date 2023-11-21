fe84b917e0d3790973ae6e731d123e11ca47db63
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-21 22:55:48 +0700

    Add formdata and isempty state

--- src/pages/dashboard/product/AddProduct.jsx
+++ src/pages/dashboard/product/AddProduct.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import HeadingTitle from "../../../components/dashboard/HeadingTitle";
 import DashboardLayout from "../../../layout/DashboardLayout";
 
 export default function AddProduct() {
+    const [formData, setFormData] = useState({
+        name: "",
+        image: null,
+        category: "",
+        price: "",
+        stock: "",
+        description: "",
+    })
+
+    const [isEmpty, setIsEmpty] = useState({
+        name: false,
+        image: false,
+        category: false,
+        price: false,
+        stock: false,
+        description: false,
+    });
+
     return (
         <DashboardLayout>
             <HeadingTitle title="Tambah Produk">
