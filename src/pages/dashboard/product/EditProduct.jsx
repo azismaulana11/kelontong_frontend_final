eb06b710c31c40f6f3dcffadbb2261f84d3f0f32
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-21 23:33:06 +0700

    Add handle validation and submit to EditProduct component

--- src/pages/dashboard/product/EditProduct.jsx
+++ src/pages/dashboard/product/EditProduct.jsx
@@ -95,6 +95,52 @@ export default function EditProduct() {
         }
     };
 
+    const handleValidation = () => {
+        setIsEmpty({
+            name: formData.name === undefined || formData.name === "",
+            image: formData.image === undefined || formData.image === "",
+            category: formData.category === undefined || formData.category === "",
+            price: formData.price === undefined || formData.price === "",
+            stock: formData.stock === undefined || formData.stock === "",
+            description: formData.description === undefined || formData.description === "",
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        handleValidation();
+        try {
+            const formDataToSend = new FormData();
+            for (const key in formData) {
+                formDataToSend.append(key, formData[key]);
+            }
+            let imageUrl = formData.image;
+            if (formData.image instanceof File) {
+                const responseUpload = await axios.post("http://localhost:5500/api/v1/upload", formDataToSend);
+                imageUrl = responseUpload.data.imageUrl;
+            }
+            const response = await axios.put(`http://localhost:5500/api/v1/products/${id}`, {
+                ...formData,
+                image: imageUrl,
+            });
+            setSwalProps({
+                show: true,
+                title: 'Sukses',
+                text: 'Data berhasil diedit',
+                icon: 'success',
+            });
+            console.log("Data berhasil diedit")
+        } catch (error) {
+            console.log("Data gagal diedit")
+            console.error({
+                statusCode: error.response.status,
+                message: error.message,
+                error: error.response.data,
+            });
+        }
+    };
+
+
     return (
         <DashboardLayout>
             <SweetAlert2 {...swalProps} />
