c14dba665e1c8587980f413754ac6322e52d934e
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-21 23:31:46 +0700

    Add useEffect hook to API Call in EditProduct component

--- src/pages/dashboard/product/EditProduct.jsx
+++ src/pages/dashboard/product/EditProduct.jsx
@@ -28,7 +28,34 @@ export default function EditProduct() {
     const [swalProps, setSwalProps] = useState({});
 
     const { id } = useParams();
-    
+
+    useEffect(() => {
+        const fetchProductData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:5500/api/v1/products/${id}`);
+                console.log(response)
+                const productData = response.data;
+                setFormData({
+                    name: productData.name,
+                    image: productData.image,
+                    category: productData.category,
+                    price: productData.price,
+                    stock: productData.stock,
+                    description: productData.description,
+                });
+                setImagePreview(`${BASE_SERVER}/upload/${productData.image}`);
+            } catch (error) {
+                console.error({
+                    statusCode: error.response.status,
+                    message: error.message,
+                    error: error.response.data,
+                });
+            }
+        };
+
+        fetchProductData();
+    }, []);
+
     return (
         <DashboardLayout>
             <SweetAlert2 {...swalProps} />
