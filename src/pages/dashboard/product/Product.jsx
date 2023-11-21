eb46c6e655a2c6e8817bd077132927c71ae388d3
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-21 21:01:02 +0700

    Add HeadingTitle component to Product page

--- src/pages/dashboard/product/Product.jsx
+++ src/pages/dashboard/product/Product.jsx
@@ -1,9 +1,12 @@
+import HeadingTitle from "../../../components/dashboard/HeadingTitle";
 import DashboardLayout from "../../../layout/DashboardLayout";
 
 export default function Product() {
-  return (
-    <DashboardLayout>
-        
-    </DashboardLayout>
-  )
+    return (
+        <DashboardLayout>
+            <HeadingTitle title="Produk">
+                <li className="breadcrumb-item active" aria-current="page" style={{ color: "black" }}>Produk</li>
+            </HeadingTitle>
+        </DashboardLayout>
+    )
 }
