c6b58144a39c1d70767cf31ee9eac936a222c4f0
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-21 23:30:31 +0700

    Add form state and validation logic to EditProduct component

--- src/pages/dashboard/product/EditProduct.jsx
+++ src/pages/dashboard/product/EditProduct.jsx
@@ -6,7 +6,30 @@ import SweetAlert2 from 'react-sweetalert2';
 import HeadingTitle from "../../../components/dashboard/HeadingTitle";
 
 export default function EditProduct() {
-   return (
+    const BASE_SERVER = "http://localhost:5500";
+    const [formData, setFormData] = useState({
+        name: "",
+        image: "",
+        category: "",
+        price: "",
+        stock: "",
+        description: "",
+    })
+    const [imagePreview, setImagePreview] = useState("");
+
+    const [isEmpty, setIsEmpty] = useState({
+        name: false,
+        image: false,
+        category: false,
+        price: false,
+        stock: false,
+        description: false,
+    });
+    const [swalProps, setSwalProps] = useState({});
+
+    const { id } = useParams();
+    
+    return (
         <DashboardLayout>
             <SweetAlert2 {...swalProps} />
         </DashboardLayout>
