ea568c05b4150c408558d7ede9d794bafd7281c6
Annisacht <unoshirazy@gmail.com>
2023-11-22 21:27:36 +0700

    perbaikan homepage login, register

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,59 +11,50 @@ function Button() {
   const dispatch = useDispatch();
 
   // Fungsi untuk logout
-const handleLogout = () => {
-  Cookies.remove('name');
-  Cookies.remove('access_token');
-  dispatch(resetLoginData());
+  const handleLogout = () => {
+    Cookies.remove('name');
+    Cookies.remove('access_token');
+    dispatch(resetLoginData());
 
-  // Tampilkan SweetAlert 2 setelah logout berhasil
-  Swal.fire({
-    icon: 'success',
-    title: 'Logout Berhasil!',
-    showConfirmButton: false,
-    timer: 1500, // Waktu notifikasi ditampilkan dalam milidetik (ms)
-  });
+    // Tampilkan SweetAlert 2 setelah logout berhasil
+    Swal.fire({
+      icon: 'success',
+      title: 'Logout Berhasil!',
+      showConfirmButton: false,
+      timer: 1500, // Waktu notifikasi ditampilkan dalam milidetik (ms)
+    });
 
-  // Redirect ke halaman utama setelah beberapa saat
-  setTimeout(() => {
-    window.location.href = '/'; // Jangan lupa mengganti sesuai kebutuhan
-  }, 1500); // Sesuaikan dengan waktu timer SweetAlert
-};
+    // Redirect ke halaman utama setelah beberapa saat
+    setTimeout(() => {
+      window.location.href = '/'; // Jangan lupa mengganti sesuai kebutuhan
+    }, 1500); // Sesuaikan dengan waktu timer SweetAlert
+  };
 
   return (
     <div className="container">
       <div className="row">
-        <div className="col ps-lg-5 justify-content-end">
+        <div className="col ps-lg-5 d-flex justify-content-end">
           {!isLoggedIn && !name && (
-            <>
-              <Link to="/login">
+            <div className="d-flex">
+              <Link to="/login" className="me-2">
                 <button type="button" className="btn btn-light border border-primary text-primary">Masuk</button>
               </Link>
               <Link to="/register">
                 <button type="button" className="btn btn-primary border border-primary text-light">Daftar</button>
               </Link>
-            </>
+            </div>
           )}
 
           {isLoggedIn && name && (
-            <>
-               {/* <!-- Example split danger button --> */}
-              <div className="container">
-                <div className="row">
-                  <div className="col">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-outline-info btn-sm">Selamat datang, {name}!</button>
-                      <button type="button" className="btn btn-outline-info dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span className="visually-hidden">Toggle Dropdown</span>
-                      </button>
-                      <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" onClick={handleLogout}>Logout</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </>
+            <div className="btn-group">
+              <button type="button" className="btn btn-outline-info btn-sm">Selamat datang, {name}!</button>
+              <button type="button" className="btn btn-outline-info dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                <span className="visually-hidden">Toggle Dropdown</span>
+              </button>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" onClick={handleLogout}>Logout</a></li>
+              </ul>
+            </div>
           )}
         </div>
       </div>
