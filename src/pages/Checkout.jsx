473ab9a51f7b6c8c86d3299b8c978c0153e8cd01
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-26 07:39:05 +0700

    Add user object to Checkout component

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,9 @@
 export default function Checkout() {
+    const user = {
+        name: "Jhon",
+        phone: "081234567890",
+        address: "Jl. Raya Bogor KM 30"
+    }
     return (
         <>
 
