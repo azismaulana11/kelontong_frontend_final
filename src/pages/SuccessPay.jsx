464043dd2b8e59ab77492b213ee3c23b645db6d5
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-30 16:08:04 +0700

    Add SuccessPay component

---
+++ src/pages/SuccessPay.jsx
@@ -0,0 +1,10 @@
+import doneImg from "../assets/img/chekout/done.svg"
+import backImg from "../assets/img/chekout/back.svg"
+import starImg from "../assets/img/chekout/star.svg"
+import productImg from "../assets/img/chekout/product-1.png"
+export default function SuccessPay() {
+    return (
+        <>
+        </>
+    )
+}
