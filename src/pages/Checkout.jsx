20e6ef29bf8af0e86a686b9d13b7dcad23c1d808
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-30 16:06:24 +0700

    Remove unnecessary code and update payment process

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -34,64 +34,6 @@ export default function Checkout() {
         getOrderById()
     }, [])
 
-    // Produk
-    const products = [
-        {
-            name: "Beras",
-            quantity: 1,
-            price: 65000,
-            image: "https://berashotel.com/wp-content/uploads/BERAS-MERAH-2KG.png"
-        },
-        {
-            name: "Gula",
-            quantity: 3,
-            price: 15000,
-            image: "https://media.monotaro.id/mid01/big/Perlengkapan%20Dapur%20%26%20Horeka/Makanan/Bahan%20Masak/Gulaku%20Gula%20Pasir%20Premium%20Putih/80P101558288-34.jpg"
-        },
-        {
-            name: "Minyak Goreng",
-            quantity: 2,
-            price: 20000,
-            image: "https://o-cdn-cas.sirclocdn.com/parenting/images/Sunco_Minyak.width-800.format-webp.webp"
-        },
-        {
-            name: "Telur",
-            quantity: 5,
-            price: 34000,
-            image: "https://www.astronauts.id/blog/wp-content/uploads/2022/08/Astro-Goods-Farmers-Omega-3-Eggs-2-1024x683.png"
-        },
-        {
-            name: "Susu",
-            quantity: 2,
-            price: 15000,
-            image: "https://www.blibli.com/friends-backend/wp-content/uploads/2023/06/B600252-4-Varian-Rasa-Susu-Zee-Vanilla-Twist.jpg"
-        },
-        {
-            name: "Teh",
-            quantity: 3,
-            price: 7000,
-            image: "https://www.tehsariwangi.com/uploads/product/7/6e27173e5f86c20a033aaeb7284da671.png"
-        },
-        {
-            name: "Kopi",
-            quantity: 2,
-            price: 15000,
-            image: "https://swamediainc.storage.googleapis.com/swa.co.id/wp-content/uploads/2020/07/21141144/Indocofe-Coffeemix.jpg"
-        },
-        {
-            name: "Sabun",
-            quantity: 5,
-            price: 16000,
-            image: "https://images.soco.id/937-soap-bar.jpg.jpeg"
-        },
-        {
-            name: "Shampoo",
-            quantity: 2,
-            price: 15000,
-            image: "https://asset-a.grid.id/crop/0x0:0x0/x/photo/2018/09/25/3603630647.jpg"
-        }
-    ]
-
     // Total belanja
     const [totalNoShipping, setTotalNoShipping] = useState(0)
     const [totalAfterShipping, setTotalAfterShipping] = useState(0)
@@ -128,12 +70,9 @@ export default function Checkout() {
     }
 
     // Midtrans
-    const [order_id, setOrder_id] = useState("")
     const [token, setToken] = useState("")
     const CLIENT_KEY_MIDTRANS = import.meta.env.VITE_APP_CLIENT_KEY_MIDTRANS;
     const processPayment = async () => {
-        const newOrderId = uuidv4()
-        setOrder_id(newOrderId)
         const data = {
             name: customer.name,
             order_id: order_id,
@@ -169,7 +108,6 @@ export default function Checkout() {
                     setToken("")
                 },
                 onPending: function (result) {
-                    localStorage.setItem("Pembayaran", JSON.stringify(result))
                     setToken("")
                 },
                 onError: function (error) {
@@ -239,15 +177,6 @@ export default function Checkout() {
                                 {customer.address}
                             </h6>
                         </div>
-                        <div className="mb-4" style={{
-                            width: "100%",
-                            height: "2px",
-                            background: "#b3b3b3",
-                            margin: "16px 0",
-                        }}></div>
-                        <button className="btn btn-outline-primary ms-3 mb-3">
-                            Pilih alamat lain
-                        </button>
                         <div className="line-2" style={{
                             width: "100%",
                             height: "10px",
