51f271108c2cebc1464a9cd568910c4e79d98413
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-30 16:08:56 +0700

    Add navigation bar to SuccessPay page

--- src/pages/SuccessPay.jsx
+++ src/pages/SuccessPay.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import doneImg from "../assets/img/chekout/done.svg"
 import backImg from "../assets/img/chekout/back.svg"
 import starImg from "../assets/img/chekout/star.svg"
@@ -5,6 +6,13 @@ import productImg from "../assets/img/chekout/product-1.png"
 export default function SuccessPay() {
     return (
         <>
+            <nav className="container-fluid p-3 navigation border-bottom border-1 border-black justify-content-between align-items-center">
+                <Link className="d-flex column-gap-2 align-items-center text-decoration-none" to={"/"}>
+                    <img src={backImg} alt="" style={{ width: "20px" }} />
+                    <div className="fw-semibold text-dark">Kembali</div>
+                </Link>
+            </nav>
+
         </>
     )
 }
